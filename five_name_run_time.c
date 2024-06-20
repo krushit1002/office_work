5c837a2e88c184720dd9c97aa0530cce344f9887
Shah Krushit <kamleshm1002@gmail.com>
2024-06-20 19:21:44 +0530

    Add files via upload

---
+++ five_name_run_time.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i,n;
+    printf("Enter the number of times the name has to print: \n");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+        {printf("B.krushit\n");}
+    return 0;
+}
